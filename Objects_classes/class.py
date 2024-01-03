cbb03fadaafdbf2a77ca60a7a0970329aefd4506
Wambu-i7 <wambuindikiru@gmail.com>
2024-01-03 06:32:32 -0800

    Attributes of the objects

--- Objects_classes/class.py
+++ Objects_classes/class.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 class CarDesign:
-    pass
-car1 = CarDesign()
-car2 = CarDesign()
-print(type(car1))
+    def __init__(self, made, plate):
+        self.company = made
+        self.number = plate
+        self.wheels = 4
+car1 = CarDesign("Toyota", "001")
+print(car1.company)
+print(car1.number)
+print(car1.wheels)
